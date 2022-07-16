9d0d8016bc880cb1d1e001e66ad1498778f26d47
Julian Hyde <jhyde@apache.org>
2022-07-16 12:22:16 -0700

    lower case keywords (suchThat to suchthat, notElem to notelem)

--- src/main/java/net/hydromatic/morel/compile/Compiler.java
+++ src/main/java/net/hydromatic/morel/compile/Compiler.java
@@ -333,7 +333,7 @@ public class Compiler {
 
     case SUCH_THAT:
       // Given
-      //   (n, d) suchThat hasNameInDept (n, d)
+      //   (n, d) suchthat hasNameInDept (n, d)
       // that is,
       //   pat = (n, d),
       //   exp = hasNameInDept (n, d),
@@ -343,7 +343,7 @@ public class Compiler {
       //       (extent: (string * int) list)
       //
       // but we'd prefer to find the extent internally, e.g. given
-      //   (n, d) suchThat (n, d) elem nameDeptPairs
+      //   (n, d) suchthat (n, d) elem nameDeptPairs
       // we generate
       //   (n, d) in nameDeptPairs
       //
@@ -895,7 +895,7 @@ public class Compiler {
     }
   }
 
-  /** A "suchThat" expression split into an extent and filters. */
+  /** A "suchthat" expression split into an extent and filters. */
   private static class ExtentFilter {
     final Core.Exp extent;
     final ImmutableList<Core.Exp> filters;
