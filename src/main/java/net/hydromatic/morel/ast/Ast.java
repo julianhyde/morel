3eb263d2424af4d33eb8ff9aa167289e7be00fd8
Julian Hyde <jhyde@apache.org>
2022-07-16 12:22:16 -0700

    lower case keywords (suchThat to suchthat, notElem to notelem)

--- src/main/java/net/hydromatic/morel/ast/Ast.java
+++ src/main/java/net/hydromatic/morel/ast/Ast.java
@@ -1641,7 +1641,7 @@ public class Ast {
         exp = ((PrefixCall) this.exp).a;
         break;
       case SUCH_THAT:
-        op = " suchThat ";
+        op = " suchthat ";
         exp = ((PrefixCall) this.exp).a;
         break;
       default:
