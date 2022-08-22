095301918f8f7fa9f2bba13f8b1bce72b8de734b
Julian Hyde <jhyde@apache.org>
2022-08-22 02:04:33 -0700

    copy stack into env when calling calcite

--- src/main/java/net/hydromatic/morel/eval/Codes.java
+++ src/main/java/net/hydromatic/morel/eval/Codes.java
@@ -3492,6 +3492,7 @@ public abstract class Codes {
       // Lazy evaluation. If code0 returns false, code1 is never evaluated.
       int status = code0.exec(stack);
       if ((boolean) stack.peek()) {
+        stack.pop();
         code1.exec(stack);
       }
       return 0;
@@ -3516,6 +3517,7 @@ public abstract class Codes {
       // Lazy evaluation. If code0 returns true, code1 is never evaluated.
       int status = code0.exec(stack);
       if (!(boolean) stack.peek()) {
+        stack.pop();
         code1.exec(stack);
       }
       return 0;
