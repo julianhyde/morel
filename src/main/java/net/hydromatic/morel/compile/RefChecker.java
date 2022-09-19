0c5c6409f1a710e9fe9de24128050568e589154d
Julian Hyde <jhyde@apache.org>
2022-09-19 10:26:41 -0700

    In EnvVisitor, improve the environment passed to Group

--- src/main/java/net/hydromatic/morel/compile/RefChecker.java
+++ src/main/java/net/hydromatic/morel/compile/RefChecker.java
@@ -22,35 +22,41 @@ import net.hydromatic.morel.ast.Core;
 import net.hydromatic.morel.type.Binding;
 import net.hydromatic.morel.type.TypeSystem;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Verify.verifyNotNull;
 
 /** Validates expressions, making sure that every {@link Core.Id}
  * exists in the environment. */
 public class RefChecker extends EnvVisitor {
   /** Creates a reference checker. */
   public static RefChecker of(TypeSystem typeSystem, Environment env) {
-    return new RefChecker(typeSystem, env);
+    return new RefChecker(typeSystem, env, new ArrayDeque<>());
   }
 
-  private RefChecker(TypeSystem typeSystem, Environment env) {
-    super(typeSystem, env);
+  private RefChecker(TypeSystem typeSystem, Environment env,
+      Deque<FromContext> fromStack) {
+    super(typeSystem, env, fromStack);
   }
 
   @Override protected EnvVisitor bind(Binding binding) {
-    return new RefChecker(typeSystem, env.bind(binding));
+    return new RefChecker(typeSystem, env.bind(binding), fromStack);
   }
 
   @Override protected EnvVisitor bind(List<Binding> bindingList) {
     // The "env2 == env" check is an optimization. If you remove it, this method
     // will have the same effect, just slower.
     final Environment env2 = env.bindAll(bindingList);
-    return env2 == env ? this : new RefChecker(typeSystem, env2);
+    if (env2 == env) {
+      return this;
+    }
+    return new RefChecker(typeSystem, env2, fromStack);
   }
 
   @Override protected void visit(Core.Id id) {
-    checkNotNull(env.getOpt(id.idPat), "not found", id);
+    verifyNotNull(env.getOpt(id.idPat), "not found", id);
   }
 }
 
