fa74d5832b01b5e21c6751a3f77dfe486fcade5c
Julian Hyde <jhyde@apache.org>
2025-02-21 16:54:49 -0800

    Start [MOREL-237] Operator overloading
    Parse 'over <id>'

--- src/main/java/net/hydromatic/morel/compile/TypeResolver.java
+++ src/main/java/net/hydromatic/morel/compile/TypeResolver.java
@@ -956,6 +956,9 @@ public class TypeResolver {
   private Ast.Decl deduceDeclType(
       TypeEnv env, Ast.Decl node, PairList<Ast.IdPat, Unifier.Term> termMap) {
     switch (node.op) {
+      case OVER_DECL:
+        return node;
+
       case VAL_DECL:
         return deduceValDeclType(env, (Ast.ValDecl) node, termMap);
 
