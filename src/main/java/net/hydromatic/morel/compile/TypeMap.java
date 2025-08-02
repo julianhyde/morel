a95021ba1e695b7435c6102c3d34b5804e50e8c8
Julian Hyde <jhyde@apache.org>
2025-08-02 14:05:33 -0700

    Checkpoint
    Includes BuiltIn.Z_AGG, ast.internalLiteral

--- src/main/java/net/hydromatic/morel/compile/TypeMap.java
+++ src/main/java/net/hydromatic/morel/compile/TypeMap.java
@@ -69,6 +69,7 @@ public class TypeMap {
     this.nodeTypeTerms = ImmutableMap.copyOf(nodeTypeTerms);
     this.substitution = requireNonNull(substitution.resolve());
     this.realTypes = ImmutableMap.copyOf(realTypes);
+    //    System.out.println(this);
   }
 
   @Override
